64d3f03b64a52486d49ddba6605360942fead112
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-04-20 04:40:26 +0600

    autoload moved to the Core directory

--- src/Core/autoload.php
+++ src/Core/autoload.php
@@ -3,8 +3,7 @@
 spl_autoload_register(function ($class) {
   // convert namespace separators to directory separators
   $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-  $class = str_replace('MintBerry', 'src', $class);
-  // echo $class . PHP_EOL;
+  $class = str_replace('Scandiweb', 'src', $class);
 
   // load the class file
   require_once __DIR__ . '/' . $class . '.php';
