533d8d1c0207d25f52bcb42406c84c377243a5b4
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-04-25 20:46:49 +0600

    new middleware added to a route

--- src/App/routes.php
+++ src/App/routes.php
@@ -4,11 +4,8 @@ use MintBerry\Core\Router;
 
 $router = new Router();
 
-$router->get('/products/index', 'ProductController@index')->middleware('TestMid');
+$router->get('/products/index', 'ProductController@index')->middleware('TestMiddleware');
 $router->get('/products/show', 'ProductController@show');
 $router->post('/products/store', 'ProductController@store');
 $router->put('/products/update', 'ProductController@update');
 $router->delete('/products/delete', 'ProductController@delete');
-
-
-$router->route();
