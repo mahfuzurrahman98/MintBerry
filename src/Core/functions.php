bc10dd236ad64cf0ae6397c03be8834d5a30e030
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-06-12 00:37:13 +0600

    some functions name changes

--- src/Core/functions.php
+++ src/Core/functions.php
@@ -18,14 +18,13 @@ function asset($folder) {
 }
 
 function csrf_token() {
-  Session::start();
   $token = bin2hex(random_bytes(32));
-  Session::put('csrf_token', $token);
+  Session::put('_csrf_token', $token);
   return $token;
 }
 
 function csrf_input() {
-  echo '<input type="hidden" name="csrf_token" value="' . csrf_token() . '">';
+  echo '<input type="hidden" name="_csrf_token" value="' . csrf_token() . '">';
 }
 
 function redirect($url) {
@@ -33,14 +32,12 @@ function redirect($url) {
   exit;
 }
 
-function redirect_with_success($url, $success) {
-  Session::start();
+function redirectWithSuccess($url, $success) {
   Session::put('success', $success);
   redirect($url);
 }
 
-function redirect_with_error($url, $error) {
-  Session::start();
+function redirectWithError($url, $error) {
   Session::put('error', $error);
   redirect($url);
 }
