61f2937d655ba8dd8d6bef3d6a2d67e8ed6450cf
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-06-12 00:39:07 +0600

    routes definded as per new syntax

--- src/App/routes.php
+++ src/App/routes.php
@@ -1,23 +1,17 @@
 <?php
 
 use MintBerry\Core\Router;
+use MintBerry\App\Controllers\ProductController;
 
 $router = new Router();
-
-// $router->get('/products/index', 'ProductController@index');
-// ->middleware('TestMiddleware');
-// $router->get('/products/show', 'ProductController@show');
-// $router->post('/products/store', 'ProductController@store');
-// $router->put('/products/update', 'ProductController@update');
-// $router->delete('/products/delete', 'ProductController@delete');
 
 // $router->get('/test', 'ProductController@test');
 // $router->post('/post-req', 'ProductController@postRreq');
 
 $router->prefix('/products', function ($router) {
-  $router->get('/index', 'ProductController@index')->middleware('TestMiddleware');
-  $router->get('/show', 'ProductController@show');
-  $router->post('/store', 'ProductController@store');
-  $router->put('/update', 'ProductController@update');
-  $router->delete('/delete', 'ProductController@delete');
+  $router->get('/index', ProductController::class, 'index');
+  $router->get('/show', ProductController::class, 'show');
+  $router->post('/store', ProductController::class, 'store');
+  $router->put('/update', ProductController::class, 'update');
+  $router->delete('/delete', ProductController::class, 'delete');
 });
