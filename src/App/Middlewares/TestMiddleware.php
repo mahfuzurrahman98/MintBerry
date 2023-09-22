8a2b0bcab3325dfdc07672c46bcd2ab7b70d3620
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2023-09-22 18:30:19 +0600

    middleware added

--- src/App/Middlewares/TestMiddleware.php
+++ src/App/Middlewares/TestMiddleware.php
@@ -3,13 +3,16 @@
 namespace MintBerry\App\Middlewares;
 
 use MintBerry\Core\Request;
+use MintBerry\Core\JSONResponse;
 
 class TestMiddleware {
+  use JSONResponse;
+
   public function handle() {
     $request = new Request();
 
     if (!$request->hasHeader('age') || $request->getHeader('age') < 18) {
-      die("not allowed");
+      $this->respondJSON(403, 'You are not allowed to access this resource');
     }
   }
 }
--- src/App/routes.php
+++ src/App/routes.php
@@ -10,7 +10,7 @@ $router->prefix('/products', function ($router) {
   $router->disableCsrfProtection();
   $router->get('/', ProductController::class, 'index');
   $router->get('/show', ProductController::class, 'show');
-  $router->post('/', ProductController::class, 'store');
+  $router->post('/', ProductController::class, 'store')->middleware('TestMiddleware');
   $router->put('/', ProductController::class, 'update');
   $router->delete('/', ProductController::class, 'delete');
 });
